b6b2a15916711e571a9f097ee9097144efd4bd65
djsmithbham <d.j.smith.2@bham.ac.uk>
2017-09-14 16:45:34 +0100

    Add files via upload
    Correction to CalcMomentFromForceDistribution related to choice of origin

--- CalcMomentFromForceDistribution.m
+++ CalcMomentFromForceDistribution.m
@@ -1,8 +1,15 @@
-function MM=CalculateMomentFromForceDistribution(f,X,X0,NN)
+function MM=CalcMomentFromForceDistribution(f,X,X0,NN)
 
-  [X1 X2 X3]=ExtractComponents(X'*NN);Ze=0*X1;
+  % modification 2017_SEP_04 to translate points before
+  % multiplying by nearest-neighbour matrix
+
+  [X1,X2,X3]=ExtractComponents(X);
   X1=X1-X0(1);
   X2=X2-X0(2);
   X3=X3-X0(3);
-  AM=[Ze -X3 X2; X3 Ze -X1; -X2 X1 Ze];
+  
+  x=[X1' X2' X3']*NN;
+  [x1,x2,x3]=ExtractComponents(x);Ze=0*x1;
+  
+  AM=[Ze -x3 x2; x3 Ze -x1; -x2 x1 Ze];
   MM=AM*f;
