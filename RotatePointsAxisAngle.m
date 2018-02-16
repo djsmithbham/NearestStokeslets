008bc1ee5a0051a2bceb499ead9c30ee997945ca
djsmithbham <d.j.smith.2@bham.ac.uk>
2018-02-16 23:18:27 +0000

    Update RotatePointsAxisAngle.m

--- RotatePointsAxisAngle.m
+++ RotatePointsAxisAngle.m
@@ -1,9 +1,9 @@
 function X=RotatePointsAxisAngle(X,rotvec)
 
 % takes general axis vector and angle as 4-vector rotvec, generates rotation matrix and performs rotation on grid vector X
-% requires simulink for vrrotvec2mat
+% vrrotvec2mat_patch no longer requires simulink 3D animation (equivalent to vrrotvec2mat)
 
   [X1 X2 X3]=ExtractComponents(X);
-  R=vrrotvec2mat(rotvec);
+  R=vrrotvec2mat_patch(rotvec);
   X=R*[(X1)';(X2)';(X3)'];
   X=reshape(X',[],1);
